c094a9f5b155f8fd3c8da2b7eae28d202e03b460
perkzen <perko.domen@gmail.com>
2023-03-08 19:00:50 +0100

    feat: delete module

--- src/common/common.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { SentryInterceptor } from './interceptors';
-
-@Global()
-@Module({
-  providers: [SentryInterceptor],
-  exports: [SentryInterceptor],
-})
-export class CommonModule {}
