842b52269b2c452490a520dd8f81be3f5ae2e97e
AmadejKrepek <krepekamadej@gmail.com>
2023-06-16 08:34:23 +0200

    Filtered data

--- src/modules/forecasts/strategies/get-weather-widget.interface.ts
+++ src/modules/forecasts/strategies/get-weather-widget.interface.ts
@@ -1,19 +1,22 @@
-import { HourlyData } from './open-meteo.strategy';
-
 export interface WeatherWidget {
   weathercode: number;
   temperature_2m_max: number;
   temperature_2m_min: number;
   sunrise: string;
   sunset: string;
-  hourly: HourlyData;
 }
 
 export interface WeatherWidgetFull extends WeatherWidget {
   description: string;
   image: string;
-  descriptionHourly: string[];
-  imageHourly: string[];
+  hourlyDataInterval: HourlyInterval;
+}
+
+export interface HourlyInterval {
+  time: string[];
+  description: string[];
+  image: string[];
+  weatherCode: number[];
 }
 
 export interface GetWeatherForecastWidgetFullInterface {
